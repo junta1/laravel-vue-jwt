39e1406c503b71c5c17c6803005877445431d4e3
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2022-09-14 01:22:15 -0300

    Adicionando filtro para login

--- vuejwt/src/App.vue
+++ vuejwt/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Login</router-link> |
-      <router-link to="/about">About</router-link>
+      JWT
     </div>
     <router-view/>
   </div>
--- vuejwt/src/router/index.js
+++ vuejwt/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import Guard from './services/middleware'
 
 Vue.use(VueRouter)
 
@@ -13,6 +14,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
+    beforeEnter: Guard.auth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
