8ac62a5d69ec95dabd143e5362c776b22bc2d7af
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2022-09-14 00:59:26 -0300

    Adicionado cookie e obtendo input por v-model

--- vuejwt/src/views/Login.vue
+++ vuejwt/src/views/Login.vue
@@ -3,13 +3,23 @@
     <form @submit.stop.prevent="submit()">
       <!-- Email input -->
       <div class="form-outline mb-4">
-        <input v-model="login.email" type="email" id="form2Example1" class="form-control" />
+        <input
+          v-model="login.email"
+          type="email"
+          id="form2Example1"
+          class="form-control"
+        />
         <label class="form-label" for="form2Example1">Email address</label>
       </div>
 
       <!-- Password input -->
       <div class="form-outline mb-4">
-        <input v-model="login.password" type="password" id="form2Example2" class="form-control" />
+        <input
+          v-model="login.password"
+          type="password"
+          id="form2Example2"
+          class="form-control"
+        />
         <label class="form-label" for="form2Example2">Password</label>
       </div>
 
@@ -22,6 +32,8 @@
 </template>
 
 <script>
+import Cookies from "js-cookie";
+
 export default {
   name: "Login",
   data() {
@@ -34,17 +46,18 @@ export default {
   },
   methods: {
     submit() {
-        console.log(this.login)
+      console.log(this.login);
       fetch(`http://localhost:31000/api/login`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Access': 'application/json',
+          "Content-Type": "application/json",
+          Access: "application/json",
         },
         body: JSON.stringify(this.login),
       })
         .then((response) => response.json())
         .then((res) => {
+          Cookies.set("_myapp_token", res.token);
           console.log(res);
         });
     },
